2bdf669258cff491ba090ea6151b187abcfba4bf
kaisapalmer <kaisa.palmer@gmail.com>
2025-10-16 11:33:19 +0200

    adding comments

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const buttonText = document.getElementById('buttonText');
 so JavaScript can interact with it */
 const para2 = document.getElementById('para2');
 
-// Making an event that changes text on a paragraph when clicking a button
+// Creates an event that changes text on a paragraph when clicking a button
 buttonText.addEventListener('click', (e) => {
   para2.innerHTML = "This is the text when you click on the button!";
 });
@@ -21,7 +21,7 @@ buttonText.addEventListener('click', (e) => {
 so JavaScript can interact with it */
 const buttonTime = document.getElementById('buttonTime');
 
-// Making an event that shows the current time when a user clicks the button
+// Creates an event that shows the current time when a user clicks the button
 buttonTime.addEventListener('click', (e) => {
   time();
 });
@@ -34,4 +34,32 @@ function time(){
                         + currentDate.getSeconds();
   document.getElementById('timeText').innerHTML = "The time is: "+timeNow;
 }
+
+// Creates a constant variable with a list of value
+const vegetables = ["Cucumber", "Tomato", "Salad", "Corn"]
+
+/* The code connects JavaScript to the HTML element that has the ID "buttonVeg"
+so JavaScript can interact with it */
+const buttonVeg = document.getElementById("buttonVeg")
+/* The code connects JavaScript to the HTML element that has the ID "listVeg"
+so JavaScript can interact with it */
+const listVeg = document.getElementById("listVeg")
+
+//Declaring a variable named "index" and giving it the value 0
+let index = 0;
+
+// Creates an event that shows one vegetable at a time when the button is clicked
+buttonVeg.addEventListener("click", () => {
+  //If there are still vegetables to show. Add one more to the list
+  if (index < vegetables.length) {
+    const li = document.createElement("li");
+    li.textContent = vegetables[index];
+    listVeg.appendChild(li);
+    index++;
+    //If all vegetables have been shown, reset the list
+  } else {
+    index = 0;
+    listVeg.innerHTML = "";
+  }
+})
 
